f79b816566dbbd4f6cb9a0ddede6c2ea726839fa
Paranoïd User <5120290+ParanoidUser@users.noreply.github.com>
2023-11-11 22:43:46 -0500

    test: basic lazy load operation

--- codewars-sdk-client/src/test/java/dev/noid/codewars/client/LazyListTest.java
+++ codewars-sdk-client/src/test/java/dev/noid/codewars/client/LazyListTest.java
@@ -1,7 +1,23 @@
 package dev.noid.codewars.client;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class LazyListTest {
 
+  @Test
+  void load_first_two_pages() {
+    List<String>[] pages = new List[] {
+        List.of("a", "b", "c", "d", "e"),
+        List.of("f", "g", "h", "i", "j"),
+        List.of("k", "l", "m", "n", "o"),
+        List.of("p", "q", "r", "s", "t")
+    };
+
+    var lazy = new LazyList<>(index -> pages[index], List.of(), 5, 10);
+    assertEquals(List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"), lazy);
+    assertEquals(10, lazy.size());
+  }
 }
